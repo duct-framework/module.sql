8996877b2bf17175d20309177539258b5485521f
James Reeves <jreeves@weavejester.com>
2017-03-09 23:22:05 +0000

    Add :duct.module/sql key

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [integrant "0.2.3"]
+                 [duct/core "0.9.0-SNAPSHOT"]
+                 [duct/database.sql.hikaricp "0.1.0-SNAPSHOT"]])
--- src/duct/module/sql.clj
+++ src/duct/module/sql.clj
@@ -1,6 +1,17 @@
-(ns duct.module.sql)
+(ns duct.module.sql
+  (:require [integrant.core :as ig]
+            [duct.core.env :refer [env]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:private default-database-url
+  (or (env "JDBC_DATABASE_URL")
+      (env "DATABASE_URL")))
+
+(defn- get-database-url [options]
+  (options :database-url default-database-url))
+
+(defn- assoc-database [config options]
+  (assoc config :duct.database.sql/hikaricp {:jdbc-url (get-database-url options)}))
+
+(defmethod ig/init-key :duct.module/sql [_ options]
+  (fn [config]
+    (assoc-database config options)))
