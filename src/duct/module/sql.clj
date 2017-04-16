6d91dd8ea0c9e873dbf01ea6b522a6d7fac93f26
James Reeves <jreeves@weavejester.com>
2017-04-16 22:06:18 +0100

    Refactor module to use merge-configs

--- src/duct/module/sql.clj
+++ src/duct/module/sql.clj
@@ -1,41 +1,34 @@
 (ns duct.module.sql
   (:require [integrant.core :as ig]
-            [duct.core :refer [assoc-in-default]]
-            [duct.core.env :refer [env]]
+            [duct.core :as core]
+            [duct.core.env :as env]
+            [duct.core.merge :as merge]
             [duct.database.sql.hikaricp :as hikaricp]
             [duct.migrator.ragtime :as ragtime]))
 
 (def ^:private default-database-url
-  (or (env "JDBC_DATABASE_URL")
-      (env "DATABASE_URL")))
+  (or (env/env "JDBC_DATABASE_URL")
+      (env/env "DATABASE_URL")))
 
 (def ^:private env-strategy
   {:production  :raise-error
    :development :rebase})
 
-(defn- get-database-url [options]
-  (options :database-url default-database-url))
+(defn- database-config [jdbc-url]
+  {:duct.database.sql/hikaricp ^:displace {:jdbc-url jdbc-url}})
 
-(defn- get-env [config options]
+(defn- migrator-config [environment]
+  {:duct.migrator/ragtime
+   {:database   ^:displace (ig/ref :duct.database/sql)
+    :strategy   (merge/displace (env-strategy environment))
+    :logger     ^:displace (ig/ref :duct/logger)
+    :migrations []}})
+
+(defn- get-environment [config options]
   (:environment options (:duct.core/environment config :production)))
-
-(defn- assoc-database [config options]
-  (assoc-in-default config
-                    [:duct.database.sql/hikaricp]
-                    {:jdbc-url (get-database-url options)}))
-
-(defn- assoc-migrator [config options]
-  (let [k (or (first (ig/find-derived-1 config :duct/migrator))
-              :duct.migrator/ragtime)]
-    (-> config
-        (assoc-in-default [k :database]   (ig/ref :duct.database/sql))
-        (assoc-in-default [k :migrations] [])
-        (assoc-in-default [k :strategy]   (env-strategy (get-env config options)))
-        (cond-> (ig/find-derived-1 config :duct/logger)
-          (assoc-in-default [k :logger]   (ig/ref :duct/logger))))))
 
 (defmethod ig/init-key :duct.module/sql [_ options]
   (fn [config]
-    (-> config
-        (assoc-database options)
-        (assoc-migrator options))))
+    (core/merge-configs config
+                        (database-config (:database-url options default-database-url))
+                        (migrator-config (get-environment config options)))))
