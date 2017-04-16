1153add3b709937be64c65e8ec7da966fc80191c
James Reeves <jreeves@weavejester.com>
2017-04-16 22:06:32 +0100

    Add basic tests

--- test/duct/module/sql_test.clj
+++ test/duct/module/sql_test.clj
@@ -1,7 +1,33 @@
 (ns duct.module.sql-test
   (:require [clojure.test :refer :all]
-            [duct.module.sql :refer :all]))
+            [duct.core :as core]
+            [duct.module.sql :as sql]
+            [integrant.core :as ig]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def base-config
+  {:duct.core/modules [(ig/ref :duct.module/sql)]
+   :duct.module/sql   {:database-url "jdbc:sqlite:"}})
+
+(deftest module-test
+  (testing "blank config"
+    (is (= (core/prep base-config)
+           (merge base-config
+                  {:duct.database.sql/hikaricp
+                   {:jdbc-url "jdbc:sqlite:"}
+                   :duct.migrator/ragtime
+                   {:database   (ig/ref :duct.database/sql)
+                    :logger     (ig/ref :duct/logger)
+                    :strategy   :raise-error
+                    :migrations []}}))))
+
+  (testing "development config"
+    (let [config (assoc base-config ::core/environment :development)]
+      (is (= (core/prep config)
+             (merge config
+                    {:duct.database.sql/hikaricp
+                     {:jdbc-url "jdbc:sqlite:"}
+                     :duct.migrator/ragtime
+                     {:database   (ig/ref :duct.database/sql)
+                      :logger     (ig/ref :duct/logger)
+                      :strategy   :rebase
+                      :migrations []}}))))))
