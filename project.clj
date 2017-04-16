21087a01ac530048830e43ba40c98b7662991800
James Reeves <jreeves@weavejester.com>
2017-04-16 18:57:51 +0100

    Update Duct dependencies

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [integrant "0.3.3"]
-                 [duct/core "0.9.0-SNAPSHOT"]
-                 [duct/database.sql.hikaricp "0.1.0-SNAPSHOT"]
-                 [duct/migrator.ragtime "0.1.0-SNAPSHOT"]])
+                 [duct/core "0.1.0"]
+                 [duct/database.sql.hikaricp "0.1.0"]
+                 [duct/migrator.ragtime "0.1.0"]])
