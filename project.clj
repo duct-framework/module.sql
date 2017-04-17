8a6d8a6b7a5bc1e31f529fb7256129bd60a270ca
James Reeves <jreeves@weavejester.com>
2017-04-17 23:28:41 +0100

    Update duct/core to 0.1.1

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [integrant "0.3.3"]
-                 [duct/core "0.1.0"]
+                 [duct/core "0.1.1"]
                  [duct/database.sql.hikaricp "0.1.0"]
                  [duct/migrator.ragtime "0.1.0"]])
