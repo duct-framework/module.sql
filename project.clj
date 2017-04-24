e0417bf812076faa4e307addd4182b528a03e3d3
James Reeves <jreeves@weavejester.com>
2017-04-25 00:59:28 +0100

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/module.sql "0.1.1"
+(defproject duct/module.sql "0.2.0"
   :description "Duct module for working with a SQL database"
   :url "https://github.com/duct-framework/module.sql"
   :license {:name "Eclipse Public License"
