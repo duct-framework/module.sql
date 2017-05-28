54a2b10368d3f4a921a5c0921a5a896f6dca6437
James Reeves <jreeves@weavejester.com>
2017-05-28 22:02:38 +0100

    Update database.sql.hikaricp to 0.1.1
    Fixes Schema warning when running under Clojure 1.9.0.

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [duct/core "0.3.3"]
-                 [duct/database.sql.hikaricp "0.1.0"]
+                 [duct/database.sql.hikaricp "0.1.1"]
                  [duct/migrator.ragtime "0.1.1"]
                  [integrant "0.4.0"]])
