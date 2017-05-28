d039dcd8452bcbc8c4a2deb3e9aa93d0ebfd040d
James Reeves <jreeves@weavejester.com>
2017-05-28 21:59:13 +0100

    Update Clojure to 1.9.0-alpha17

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/module.sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [duct/core "0.2.1"]
                  [duct/database.sql.hikaricp "0.1.0"]
                  [duct/migrator.ragtime "0.1.1"]
