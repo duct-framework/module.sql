1bc22a63a7b1cd8381042372284d0719c99afa47
James Reeves <jreeves@weavejester.com>
2017-06-02 19:58:06 +0100

    Update duct/core to 0.4.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [duct/core "0.3.3"]
+                 [duct/core "0.4.0"]
                  [duct/database.sql.hikaricp "0.1.2"]
                  [duct/migrator.ragtime "0.1.2"]
                  [integrant "0.4.0"]])
