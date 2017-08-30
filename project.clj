d9ed27192affa48058efc37e648509cac9123572
James Reeves <jreeves@weavejester.com>
2017-08-30 04:31:01 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/duct-framework/module.sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [duct/core "0.6.0"]
-                 [duct/database.sql.hikaricp "0.2.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+                 [duct/core "0.6.1"]
+                 [duct/database.sql.hikaricp "0.3.0"]
                  [duct/migrator.ragtime "0.2.0"]
                  [integrant "0.6.1"]])
