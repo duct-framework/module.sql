2919d586a95f9190bcc9834402f8e4f3a6955fb3
James Reeves <jreeves@weavejester.com>
2017-09-01 04:36:41 +0100

    Update duct/database.sql.hikaricp to 0.3.1
    Fixes bug with closing datasource.

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
                  [duct/core "0.6.1"]
-                 [duct/database.sql.hikaricp "0.3.0"]
+                 [duct/database.sql.hikaricp "0.3.1"]
                  [duct/migrator.ragtime "0.2.0"]
                  [integrant "0.6.1"]])
