294c9a7e0d0958327c0ac28bf5e5f03d20c6911d
James Reeves <jreeves@weavejester.com>
2018-12-03 10:09:21 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [duct/core "0.7.0-alpha5"]
+                 [duct/core "0.7.0-beta2"]
                  [duct/database.sql.hikaricp "0.4.0-alpha1"]
-                 [duct/migrator.ragtime "0.2.2"]
-                 [integrant "0.7.0-alpha2"]
+                 [duct/migrator.ragtime "0.3.0-beta1"]
+                 [integrant "0.7.0"]
                  [medley "1.0.0"]])
