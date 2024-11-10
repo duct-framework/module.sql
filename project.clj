3d239b09cff233f423bd7d1d28260e668f28a104
James Reeves <jreeves@weavejester.com>
2024-11-10 16:20:08 +0000

    Update migrator.ragtime dependency

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.4"]
                  [org.duct-framework/database.sql.hikaricp "0.7.0"]
-                 [org.duct-framework/migrator.ragtime "0.4.0"]
+                 [org.duct-framework/migrator.ragtime "0.5.0"]
                  [integrant "0.13.1"]])
