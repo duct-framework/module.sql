0d64bfbd8e764ba99177e8a2739b092b8100afd8
James Reeves <jreeves@weavejester.com>
2024-12-06 03:15:18 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.duct-framework/database.sql.hikaricp "0.7.0"]
-                 [org.duct-framework/migrator.ragtime "0.5.1"]
+                 [org.duct-framework/migrator.ragtime "0.5.2"]
                  [org.duct-framework/repl.refers "0.1.0"]
                  [integrant "0.13.1"]
                  [com.github.seancorfield/next.jdbc "1.3.967"]]
   :profiles
-  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.47.0.0"]
+  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.47.1.0"]
                         [integrant/repl "0.4.0"]]}})
