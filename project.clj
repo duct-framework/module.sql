655eff4fc9b9b95ef5c9f79383523c6148036984
James Reeves <jreeves@weavejester.com>
2025-09-14 21:33:19 +0100

    Release 0.8.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.duct-framework/module.sql "0.8.0"
+(defproject org.duct-framework/module.sql "0.8.1"
   :description "Duct module for working with a SQL database"
   :url "https://github.com/duct-framework/module.sql"
   :license {:name "Eclipse Public License"
